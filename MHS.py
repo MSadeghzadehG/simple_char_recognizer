d09224d6bcf5a0225267fb435db08a98053d3da1
Mahdi Sadeghzadeh Ghamsary <mahdisadeghzadeh24@gmail.com>
2019-06-08 16:27:17 +0430

    final

--- MHS.py
+++ MHS.py
@@ -1,8 +1,14 @@
 from skimage.io import imread, imsave
 from skimage.filters import threshold_otsu
 from skimage import img_as_ubyte
-from keras.models import load_model
-from skimage.transform import rescale, resize, downscale_local_mean
+from kegit ras.models import load_model
+# from skimage.transform import rescale, resize, downscale_local_mean
+from skimage.transform import resize
+import numpy as np
+
+import os,cv2
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+
 
 
 def binarization(input_image):
@@ -14,14 +20,23 @@ car_image = imread("captcha.jpg", as_grey=True)
 binary_car_image = binarization(car_image * 255)
 # print(car_image.shape)
 
-cv_image = img_as_ubyte(binary_car_image)
 
 
 
 model = load_model('my_model.h5')
 for i in range(3):
-    char = cv_image[:, i*15:(i+1)*15]
-    print(char.shape)
-    char = char.reshape(1, 28, 28, 1)/255    
+    char = binary_car_image[:, i*15:(i+1)*15]
+    # print(char.shape)
+    char = resize(char, (28, 28), anti_aliasing=False)
+    # imshow(char)
+    char = binarization(char)
+    char = img_as_ubyte(char)
+    # cv2.imshow('image', char)
+    # cv2.waitKey()
+    # cv2.destroyAllWindows()
+    char = char.reshape(1, 28, 28, 1)
+    char = char.astype('float32')
+    # char /= 255    
     out = model.predict(char)
-    print(out)
+    # print(out)
+    print(str(np.argmax(out)))
